f8906c3176cbbe1942dd614fee6125b5a3a340ba
Javier Eguiluz <javier.eguiluz@gmail.com>
2012-09-17 19:33:37 +0200

    Actualizado el contenido de alguno de los archivos principales del framework para que sean iguales que los de Symfony 2.1

--- web/app.php
+++ web/app.php
@@ -1,12 +1,24 @@
 <?php
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
+use Symfony\Component\ClassLoader\ApcClassLoader;
+use Symfony\Component\HttpFoundation\Request;
+
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
+// Use APC for autoloading to improve performance
+// Change 'sf2' by the prefix you want in order to prevent key conflict with another application
+/*
+$loader = new ApcClassLoader('sf2', $loader);
+$loader->register(true);
+*/
+
 require_once __DIR__.'/../app/AppKernel.php';
 require_once __DIR__.'/../app/AppCache.php';
-
-use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 $kernel = new AppCache($kernel);
-$kernel->handle(Request::createFromGlobals())->send();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
--- web/app_dev.php
+++ web/app_dev.php
@@ -1,24 +1,30 @@
 <?php
 
-libxml_use_internal_errors(true);
+use Symfony\Component\HttpFoundation\Request;
 
-// this check prevents access to debug front controllers that are deployed by accident to production servers.
-// feel free to remove this, extend it, or make something more sophisticated.
-if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
-    '127.0.0.1',
-    '::1',
-))) {
+// If you don't want to setup permissions the proper way, just uncomment the following PHP line
+// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
+//umask(0000);
+
+// This check prevents access to debug front controllers that are deployed by accident to production servers.
+// Feel free to remove this, extend it, or make something more sophisticated.
+if (isset($_SERVER['HTTP_CLIENT_IP'])
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !in_array(@$_SERVER['REMOTE_ADDR'], array(
+        '127.0.0.1',
+        '::1',
+    ))
+) {
     header('HTTP/1.0 403 Forbidden');
-    die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
-require_once __DIR__.'/../app/AppCache.php';
-use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
-$kernel = new AppCache($kernel);
-$kernel->handle(Request::createFromGlobals())->send();
-
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
