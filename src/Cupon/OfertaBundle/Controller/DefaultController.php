c7d7a5dcc832f72f43092e3e6236cc423233f63f
Javier Eguiluz <javier.eguiluz@gmail.com>
2012-10-04 12:57:25 +0200

    Mejorada la ruta de la portada
    Symfony 2.1 permite utilizar parámetros del contenedor de inyección
    de dependencias directamente en las rutas

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -22,12 +22,6 @@ class DefaultController extends Controller
      */
     public function portadaAction($ciudad)
     {
-        if (null == $ciudad) {
-            $ciudad = $this->container->getParameter('cupon.ciudad_por_defecto');
-
-            return new RedirectResponse($this->generateUrl('portada', array('ciudad' => $ciudad)));
-        }
-
         $em = $this->getDoctrine()->getEntityManager();
         $oferta = $em->getRepository('OfertaBundle:Oferta')->findOfertaDelDia($ciudad);
 
