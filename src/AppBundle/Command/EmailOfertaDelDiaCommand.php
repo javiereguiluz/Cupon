e4c0b851510c2c6b32f6841200046423f868a340
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-03-28 20:59:41 +0200

    Actualizado el comando de consola

--- src/AppBundle/Command/EmailOfertaDelDiaCommand.php
+++ src/AppBundle/Command/EmailOfertaDelDiaCommand.php
@@ -14,6 +14,8 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Comando que envía cada día un email a todos los usuarios que lo
@@ -21,10 +23,19 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class EmailOfertaDelDiaCommand extends ContainerAwareCommand
 {
+    private $host;
+    private $accion;
+    /** @var ContainerInterface */
+    private $contenedor;
+    /** @var ObjectManager */
+    private $em;
+    /** @var SymfonyStyle */
+    private $io;
+
     protected function configure()
     {
         $this
-            ->setName('email:oferta-del-dia')
+            ->setName('app:email:oferta-del-dia')
             ->setDefinition(array(
                 new InputOption('accion', false, InputOption::VALUE_OPTIONAL, 'Indica si los emails realmente se envían a sus destinatarios o sólo se generan'),
             ))
@@ -37,53 +48,84 @@ EOT
         );
     }
 
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->host = 'dev' === $input->getOption('env') ? 'http://cupon.local' : 'http://cupon.com';
+        $this->accion = $input->getOption('accion');
+        $this->contenedor = $this->getContainer();
+        $this->em = $this->contenedor->get('doctrine')->getManager();
+        $this->io = new SymfonyStyle($input, $output);
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $host = 'dev' === $input->getOption('env') ? 'http://cupon.local' : 'http://cupon.com';
-        $accion = $input->getOption('accion');
+        $this->io->title('Mailing - Oferta del día');
 
-        $contenedor = $this->getContainer();
-        $em = $contenedor->get('doctrine')->getManager();
+        $destinatarios = $this->em->getRepository('AppBundle:Usuario')->findBy(array('permiteEmail' => true));
+        $this->io->text(sprintf('Se van a enviar <info>%d</info> emails', count($destinatarios)));
 
-        // Obtener el listado de usuarios que permiten el envío de email
-        $usuarios = $em->getRepository('UsuarioBundle:Usuario')->findBy(array('permite_email' => true));
+        $ofertasDelDia = $this->findOfertasDelDia();
+        foreach ($destinatarios as $destinatario) {
+            $ciudad = $destinatario->getCiudad();
+            $oferta = $ofertasDelDia[$ciudad->getId()];
 
-        $output->writeln(sprintf(' Se van a enviar <info>%s</info> emails', count($usuarios)));
+            $contenido = $this->contenedor->get('twig')->render('email/oferta_del_dia.html.twig', array(
+                'host' => $this->host,
+                'ciudad' => $ciudad,
+                'oferta' => $oferta,
+                'usuario' => $destinatario
+            ));
 
-        // Buscar la 'oferta del día' en todas las ciudades de la aplicación
+            $asunto = sprintf('[Oferta del día] %s en %s', $oferta->getNombre(), $oferta->getTienda()->getNombre());
+            $this->enviarEmail($destinatario, $asunto, $contenido);
+        }
+
+        if ('enviar' !== $this->accion) {
+            $this->io->comment(array(
+                'NOTA: No se ha enviado ningún email.',
+                'Para enviar los emails a sus destinatarios, ejecuta el comando con la opción <info>accion</info>.',
+                'Ejemplo: <info>./app/console email:oferta-del-dia --accion=enviar</info>'
+            ));
+        }
+
+        $this->io->success(sprintf('%d emails enviados con la oferta del día', count($destinatarios)));
+    }
+
+    /**
+     * Busca la 'oferta del día' en todas las ciudades de la aplicación
+     *
+     * @return array
+     */
+    private function findOfertasDelDia()
+    {
         $ofertas = array();
-        $ciudades = $em->getRepository('AppBundle:Ciudad')->findAll();
+        $ciudades = $this->em->getRepository('AppBundle:Ciudad')->findAll();
         foreach ($ciudades as $ciudad) {
             $id = $ciudad->getId();
             $slug = $ciudad->getSlug();
 
-            $ofertas[$id] = $em->getRepository('AppBundle:Oferta')->findOfertaDelDiaSiguiente($slug);
+            $ofertas[$id] = $this->em->getRepository('AppBundle:Oferta')->findOfertaDelDiaSiguiente($slug);
         }
 
-        // Generar el email personalizado de cada usuario
-        foreach ($usuarios as $usuario) {
-            $ciudad = $usuario->getCiudad();
-            $oferta = $ofertas[$ciudad->getId()];
+        return $ofertas;
+    }
 
-            $contenido = $contenedor->get('twig')->render(
-                'BackendBundle:Oferta:email.html.twig',
-                array('host' => $host, 'ciudad' => $ciudad, 'oferta' => $oferta, 'usuario' => $usuario)
-            );
+    /**
+     * @param string $destinatario
+     * @param string $asunto
+     * @param string $contenido
+     */
+    private function enviarEmail($destinatario, $asunto, $contenido)
+    {
+        if ('enviar' === $this->accion) {
+            $email = \Swift_Message::newInstance()
+                ->setSubject($asunto)
+                ->setFrom(array('oferta-del-dia@cupon.com' => 'Cupon - Oferta del día'))
+                ->setTo($destinatario->getEmail())
+                ->setBody($contenido, 'text/html')
+            ;
 
-            // Enviar el email
-            if ('enviar' === $accion) {
-                $email = \Swift_Message::newInstance()
-                    ->setSubject($oferta->getNombre().' en '.$oferta->getTienda()->getNombre())
-                    ->setFrom(array('oferta-del-dia@cupon.com' => 'Cupon - Oferta del día'))
-                    ->setTo($usuario->getEmail())
-                    ->setBody($contenido, 'text/html')
-                ;
-                $this->getContainer()->get('mailer')->send($email);
-            }
-        }
-
-        if ('enviar' !== $accion) {
-            $output->writeln("\n No se ha enviado ningún email. Para enviar los emails a sus destinatarios,\n ejecuta el comando con la opción <info>accion</info>. Ejemplo:\n <info>./app/console email:oferta-del-dia --accion=enviar</info>\n");
+            $this->contenedor->get('mailer')->send($email);
         }
     }
 }
