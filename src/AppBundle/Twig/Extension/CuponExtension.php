f48c8478da2dc4db4fcdd5d11251a450f3256cb8
Javier Eguiluz <javier.eguiluz@gmail.com>
2016-03-28 12:00:54 +0200

    Usada la extensión Intl de Twig en vez de nuestro propio filtro "fecha"

--- src/AppBundle/Twig/Extension/CuponExtension.php
+++ src/AppBundle/Twig/Extension/CuponExtension.php
@@ -10,24 +10,11 @@ use Symfony\Component\Translation\TranslatorInterface;
  */
 class CuponExtension extends \Twig_Extension
 {
-    private $translator;
-
-    public function __construct(TranslatorInterface $translator = null)
-    {
-        $this->translator = $translator;
-    }
-
-    public function getTranslator()
-    {
-        return $this->translator;
-    }
-
     public function getFilters()
     {
         return array(
             new \Twig_SimpleFilter('mostrar_como_lista', array($this, 'mostrarComoLista'), array('is_safe' => array('html'))),
             new \Twig_SimpleFilter('cuenta_atras', array($this, 'cuentaAtras'), array('is_safe' => array('html'))),
-            new \Twig_SimpleFilter('fecha', array($this, 'fecha')),
         );
     }
 
@@ -96,49 +83,6 @@ class CuponExtension extends \Twig_Extension
 EOJ;
 
         return $html;
-    }
-
-    /**
-     * Formatea la fecha indicada según las características del locale seleccionado.
-     * Se utiliza para mostrar correctamente las fechas en el idioma de cada usuario.
-     *
-     * @param string $fecha        Objeto que representa la fecha original
-     * @param string $formatoFecha Formato con el que se muestra la fecha
-     * @param string $formatoHora  Formato con el que se muestra la hora
-     * @param string $locale       El locale al que se traduce la fecha
-     */
-    public function fecha($fecha, $formatoFecha = 'medium', $formatoHora = 'none', $locale = null)
-    {
-        // Código copiado de
-        //   https://github.com/thaberkern/symfony/blob
-        //   /b679a23c331471961d9b00eb4d44f196351067c8
-        //   /src/Symfony/Bridge/Twig/Extension/TranslationExtension.php
-
-        // Formatos: http://www.php.net/manual/en/class.intldateformatter.php#intl.intldateformatter-constants
-        $formatos = array(
-            // Fecha/Hora: (no se muestra nada)
-            'none' => \IntlDateFormatter::NONE,
-            // Fecha: 12/13/52  Hora: 3:30pm
-            'short' => \IntlDateFormatter::SHORT,
-            // Fecha: Jan 12, 1952  Hora:
-            'medium' => \IntlDateFormatter::MEDIUM,
-            // Fecha: January 12, 1952  Hora: 3:30:32pm
-            'long' => \IntlDateFormatter::LONG,
-            // Fecha: Tuesday, April 12, 1952 AD  Hora: 3:30:42pm PST
-            'full' => \IntlDateFormatter::FULL,
-        );
-
-        $formateador = \IntlDateFormatter::create(
-            $locale != null ? $locale : $this->getTranslator()->getLocale(),
-            $formatos[$formatoFecha],
-            $formatos[$formatoHora]
-        );
-
-        if ($fecha instanceof \DateTime) {
-            return $formateador->format($fecha);
-        } else {
-            return $formateador->format(new \DateTime($fecha));
-        }
     }
 
     /**
